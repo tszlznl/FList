5931763bd66813e607d8a6566d7f219724ec7916
tszlznl <139991256+tszlznl@users.noreply.github.com>
2024-08-19 01:18:58 +0800

    Update vuepress.config.ts

--- vuepress.config.ts
+++ vuepress.config.ts
@@ -21,9 +21,9 @@ export default defineUserConfig({
   lang: 'zh-CN',
   public: `./public`,
   // 网站标题，标题颜色可在 src/client/css/main.css 中修改
-  title: 'FList',
+  title: 'Glist',
   // 网站的简介，有助于搜索引擎收录
-  description: 'FList - 将 GitHub Releases 以类似网盘的形式展示在网页上，方便用户下载开源软件。 支持视频、音频、图片、PDF 等文件的在线预览。',
+  description: 'Glist - 将 GitHub Releases 以类似网盘的形式展示在网页上，方便用户下载开源软件。 支持视频、音频、图片、PDF 等文件的在线预览。',
   // 页面 <head> 标签内添加的额外标签。 不要修改/logo.png可以替换掉这个文件，删除logo.png会导致构建出错。
   head: [['link', { rel: 'icon', href: '/logo.png' }]],
   // 页面预加载，所有其它页面所需的文件都会被预拉取。这对于小型站点来说是十分有帮助的，因为它会大大提升页面切换的速度。但是在你的网站有很多页面时不建议你这么做。
@@ -89,7 +89,7 @@ export default defineUserConfig({
     {
       mountPath: "/ProgrammingVTuberLogos",
       analysis: githubReposAnalysis({
-        user: "Aikoyori",
+        user: "tszlznl",
         repository: "ProgrammingVTuberLogos",
       }),
       downProxy: cloudflarePagesDownProxy()
